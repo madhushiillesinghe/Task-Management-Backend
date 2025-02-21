73a8e80cd865017be4eb08101f7810ad4ae75921
madhuille11 <madhushiillesinghe225@gmail.com>
2025-02-21 09:42:19 +0530

    task filtered bug fixed

--- src/controller/task/taskController.ts
+++ src/controller/task/taskController.ts
@@ -41,14 +41,16 @@ export const getTasks = asyncHandler(async (req: Request, res: Response): Promis
   try {
     const userId = (req.user as IUser)._id; // Get the user ID from the request
 
-    // Find tasks associated with the current user
-    const tasks = await TaskModel.find();
+    // Find tasks that belong to the authenticated user
+    const tasks = await TaskModel.find({ user: userId });
+
     res.status(200).json({ length: tasks.length, tasks });
   } catch (error: any) {
     console.error("Error fetching tasks:", error.message);
-     res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message });
   }
 });
+
 
 // Get a single task by ID
 export const getTask = asyncHandler(async (req: Request, res: Response): Promise<void> => {
